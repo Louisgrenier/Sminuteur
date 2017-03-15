e72c8468b1c9b861dd80c8a495b88aad2405c729
Louis GRENIER <l.grenier@arsn.fr>
2017-03-15 02:17:13 +0100

    Modeling + code cercle ok

--- Minuteur/Minuteur.Designer.vb
+++ Minuteur/Minuteur.Designer.vb
@@ -22,45 +22,29 @@ Partial Class Minuteur
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-<<<<<<< HEAD
+        Me.LblChrono = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-=======
-        Me.LblAfficheur = New System.Windows.Forms.Label()
-        Me.SuspendLayout()
+        'LblChrono
         '
-        'LblAfficheur
+        Me.LblChrono.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LblChrono.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblChrono.Location = New System.Drawing.Point(0, 0)
+        Me.LblChrono.Name = "LblChrono"
+        Me.LblChrono.Size = New System.Drawing.Size(316, 307)
+        Me.LblChrono.TabIndex = 0
+        Me.LblChrono.Text = "Label1"
+        Me.LblChrono.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        Me.LblAfficheur.BackColor = System.Drawing.Color.Transparent
-        Me.LblAfficheur.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.LblAfficheur.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LblAfficheur.Location = New System.Drawing.Point(0, 0)
-        Me.LblAfficheur.Name = "LblAfficheur"
-        Me.LblAfficheur.Size = New System.Drawing.Size(316, 307)
-        Me.LblAfficheur.TabIndex = 0
-        Me.LblAfficheur.Text = "Label1"
-        Me.LblAfficheur.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
->>>>>>> Modeling
         'Minuteur
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-<<<<<<< HEAD
-        Me.Name = "Minuteur"
-        Me.Size = New System.Drawing.Size(842, 463)
-        Me.ResumeLayout(False)
-
-    End Sub
-=======
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.Controls.Add(Me.LblAfficheur)
+        Me.Controls.Add(Me.LblChrono)
         Me.Name = "Minuteur"
         Me.Size = New System.Drawing.Size(316, 307)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents LblAfficheur As Windows.Forms.Label
->>>>>>> Modeling
+    Friend WithEvents LblChrono As Windows.Forms.Label
 End Class
