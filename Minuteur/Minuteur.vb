7d79d24ae7a0b65f9997fdf52099b08cdda857a0
Louis GRENIER <l.grenier@arsn.fr>
2017-03-15 01:11:27 +0100

    propriété evol

--- Minuteur/Minuteur.vb
+++ Minuteur/Minuteur.vb
@@ -1,9 +1,10 @@
-﻿Imports System.Drawing
+﻿Imports System.ComponentModel
+Imports System.Drawing
 Imports System.Windows.Forms
 
 Public Class Minuteur
 
-
+#Region "Code"
 
     Protected Overrides Sub onpaint(e As PaintEventArgs)
 
@@ -22,4 +23,34 @@ Public Class Minuteur
         Invalidate()
 
     End Sub
+#End Region
+
+#Region "propriété"
+
+    <Description("Start ou Stop horloge")>
+    Public Property Etat As Boolean
+
+        Get
+            Return Etat
+        End Get
+
+        Set(value As Boolean)
+            Etat = value
+        End Set
+    End Property
+
+    <Description("Temps minute")>
+    Public Property temps As Boolean
+
+        Get
+            Return Etat
+        End Get
+
+        Set(value As Single)
+            Etat = value
+        End Set
+    End Property
+
+#End Region
+
 End Class
