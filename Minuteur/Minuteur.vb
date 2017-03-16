f137465d85535645d0ac4df975f2e71526d69e08
Louis GRENIER <l.grenier@arsn.fr>
2017-03-16 14:19:31 +0100

    Version V 1.0
    fin du développement version 1 
    dll fonctionnel

--- Minuteur/Minuteur.vb
+++ Minuteur/Minuteur.vb
@@ -7,32 +7,25 @@ Public Class Minuteur
 
 #Region "variable global"
     Dim m_etat As String
-    Dim minutes As Integer = 0
-    Dim seconde As Integer = 0
+    Dim minutes As Single = 0
+    Dim seconde As Single = 0
+    Dim calc_sec As Single
+    Dim max_sec As Single
+
+    Dim Mem_min As Single
     Public Enum EtatChrono
         Play = 1
         Pause = 0
     End Enum
 #End Region
 
-#Region "Code"
-
-    Protected Overrides Sub onpaint(e As PaintEventArgs)
-
-        Dim p As New Pen(Color.Green, 10)
-
-        'Create rectangle objects
-        Dim rt As New Rectangle(2, 2, Size.Width, Size.Height)
-
-        'Draw arcs
-        e.Graphics.DrawArc(p, rt, 0, 360)
-
-    End Sub
+#Region "Code evenement form"
 
     Private Sub Chrono_Tick(sender As Object, e As EventArgs) Handles Chrono.Tick
 
         If seconde <> 0 Then
             seconde = seconde - 1
+            calc_sec = calc_sec - 1
         ElseIf seconde = 0 Then
             minutes = minutes - 1
 
@@ -44,6 +37,8 @@ Public Class Minuteur
             Chrono.Stop()
         End If
 
+        Invalidate()
+
     End Sub
 
     Private Sub Minuteur_Load(sender As Object, e As EventArgs) Handles Me.Load
@@ -54,6 +49,35 @@ Public Class Minuteur
         seconde = 0
 
     End Sub
+
+#End Region
+
+#Region "Sub sans evenement"
+
+    Protected Overrides Sub onpaint(e As PaintEventArgs)
+
+        Dim p As New Pen(Color.Green, 5)
+
+        'Create rectangle objects
+        Dim rt As New Rectangle(2, 2, (Size.Width - 4), (Size.Height - 4))
+
+        'Draw arcs selon fonction de calcul d'angle
+        e.Graphics.DrawArc(p, rt, -90, CalcAff)
+
+    End Sub
+
+    Private Function CalcAff() As Single
+        'calcul d'angle selon les seconde
+        '360° -> max sec 
+        ' ?   -> x sec
+
+        Dim angle As Single
+
+        angle = (calc_sec * 360) / max_sec
+
+        Return angle
+
+    End Function
 
 #End Region
 
@@ -77,15 +101,19 @@ Public Class Minuteur
     End Property
 
     <Description("Temps minute")>
-    Public Property Temporisation As Integer
+    Public Property Temporisation As Single
 
         Get
             Return Temporisation
         End Get
 
-        Set(value As Integer)
-            Temporisation = value
+        Set(value As Single)
+
+            Mem_min = value
             minutes = value
+            max_sec = 60 * minutes
+            calc_sec = max_sec
+
         End Set
     End Property
 
